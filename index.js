b0ea10b497dff1bce6e3d5fdce4072ea5f37f4a4
wael kamira <ramondm4@gmail.com>
2023-04-04 17:35:29 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return 'This one is on me!'
+  } else if (distance > 400 && distance <= 2000 ) {
+    return 'That will be twenty bucks.'
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
+    
+  
+}
+scuberGreetingForFeet(2501)
+
+
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+
+return city === 'NYC'? `Ok, sounds good.`: `No go.`;
 }
 
-function ternaryCheckCity(){
+ternaryCheckCity ('Pittsburgh')
+
+
+
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous' :
+      return 'Thank you.';
+    default :
+    return 'Bye.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip('generous');
